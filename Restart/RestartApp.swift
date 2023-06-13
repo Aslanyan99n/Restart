fd7986ba7a6998355767e4234afeb2e9a0d46f7b
Aslanyan99n <a99narek@gmail.com>
2023-06-13 09:09:03 +0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Narek on 13.06.23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
